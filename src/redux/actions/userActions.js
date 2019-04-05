01327ab0be4ec5acc0012abfe3e82ea0864e95ec
Ion Batîr <ionbatir@gmail.com>
2019-04-06 01:54:16 +0300

    Add redux auth

---
+++ src/redux/actions/userActions.js
@@ -0,0 +1,107 @@
+import { auth, User } from "firebase";
+import {
+  LOGIN_USER_START,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  RESET_PASSWORD_START,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_ERROR,
+  REGISTER_USER_START,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  SIGN_OUT_USER_START,
+  SIGN_OUT_USER_SUCCESS,
+  SIGN_OUT_USER_ERROR
+} from "./types";
+
+export const login = (
+  { email, password },
+  successCallback,
+  errorCallback
+) => dispatch => {
+  dispatch({ type: LOGIN_USER_START });
+  auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(response => {
+      dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: {
+          displayName: response.user.displayName,
+          email: response.user.email,
+          phoneNumber: response.user.phoneNumber,
+          photoURL: response.user.photoURL
+        }
+      });
+      successCallback();
+    })
+    .catch(error => {
+      dispatch({ type: LOGIN_USER_ERROR, payload: { error } });
+      errorCallback();
+    });
+};
+
+export const resetPassword = (
+  { email },
+  successCallback,
+  errorCallback
+) => dispatch => {
+  dispatch({ type: RESET_PASSWORD_START });
+  auth()
+    .sendPasswordResetEmail(email)
+    .then(response => {
+      console.log("resetPassword: ", response);
+      dispatch({ type: RESET_PASSWORD_SUCCESS, payload: {} });
+      successCallback();
+    })
+    .catch(error => {
+      console.log("resetPassword: ", error);
+      dispatch({ type: RESET_PASSWORD_ERROR, payload: { error } });
+      errorCallback();
+    });
+};
+
+export const register = (
+  { email, password },
+  successCallback,
+  errorCallback
+) => dispatch => {
+  dispatch({ type: REGISTER_USER_START });
+  auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(response => {
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: {
+          displayName: response.user.displayName,
+          email: response.user.email,
+          phoneNumber: response.user.phoneNumber,
+          photoURL: response.user.photoURL,
+          accessToken: response.user.stsTokenManager.accessToken,
+          expirationTime: response.user.stsTokenManager.expirationTime,
+          refreshToken: response.user.stsTokenManager.refreshToken,
+          uid: response.user.stsTokenManager.uid
+        }
+      });
+      successCallback();
+    })
+    .catch(error => {
+      dispatch({ type: REGISTER_USER_ERROR, payload: { error } });
+      errorCallback();
+    });
+};
+
+export const signOut = (successCallback, errorCallback) => dispatch => {
+  dispatch({ type: SIGN_OUT_USER_START });
+  auth()
+    .signOut()
+    .then(response => {
+      console.log("signOut: ", response);
+      dispatch({ type: SIGN_OUT_USER_SUCCESS, payload: {} });
+      successCallback();
+    })
+    .catch(error => {
+      console.log("signOut error: ", error);
+      dispatch({ type: SIGN_OUT_USER_ERROR, payload: { error } });
+      errorCallback();
+    });
+};
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,7 +1,108 @@
-const initialValues = {};
+import {
+  LOGIN_USER_START,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
+  RESET_PASSWORD_START,
+  RESET_PASSWORD_SUCCESS,
+  RESET_PASSWORD_ERROR,
+  REGISTER_USER_START,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_ERROR,
+  SIGN_OUT_USER_START,
+  SIGN_OUT_USER_SUCCESS,
+  SIGN_OUT_USER_ERROR
+} from "../actions/types";
+
+const initialValues = {
+  login: {
+    loading: false,
+    error: null
+  },
+  resetPassword: {
+    loading: false,
+    error: null
+  },
+  register: {
+    loading: false,
+    error: null
+  },
+  signOut: {
+    loading: false,
+    error: null
+  },
+  displayName: null,
+  email: null,
+  phoneNumber: null,
+  photoURL: null
+};
 
 export default (state = initialValues, action) => {
   switch (action.type) {
+    case LOGIN_USER_START:
+      return { ...state, login: { ...state.login, loading: true } };
+    case LOGIN_USER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        login: { ...state.login, loading: false }
+      };
+    case LOGIN_USER_ERROR:
+      return {
+        ...state,
+        login: { ...state.login, loading: false, error: action.payload.error }
+      };
+    case RESET_PASSWORD_START:
+      return {
+        ...state,
+        resetPassword: { ...state.resetPassword, loading: true }
+      };
+    case RESET_PASSWORD_SUCCESS:
+      return {
+        ...state,
+        resetPassword: { ...state.resetPassword, loading: false }
+      };
+    case RESET_PASSWORD_ERROR:
+      return {
+        ...state,
+        resetPassword: {
+          ...state.resetPassword,
+          loading: false,
+          error: action.payload.error
+        }
+      };
+    case REGISTER_USER_START:
+      return { ...state, register: { ...state.register, loading: true } };
+    case REGISTER_USER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        register: { ...state.register, loading: false }
+      };
+    case REGISTER_USER_ERROR:
+      return {
+        ...state,
+        register: {
+          ...state.register,
+          loading: false,
+          error: action.payload.error
+        }
+      };
+    case SIGN_OUT_USER_START:
+      return {
+        ...state,
+        signOut: { ...state.signOut, loading: true }
+      };
+    case SIGN_OUT_USER_SUCCESS:
+      return { ...state, signOut: { ...state.signOut, loading: false } };
+    case SIGN_OUT_USER_ERROR:
+      return {
+        ...state,
+        signOut: {
+          ...state.signOut,
+          loading: false,
+          error: action.payload.error
+        }
+      };
     default:
       return state;
   }
