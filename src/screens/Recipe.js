aef0ec11415ed27a86a791252fa56720568d69f7
Ion Batîr <ionbatir@gmail.com>
2019-04-06 17:29:11 +0300

    Fetch recipe

--- src/screens/Recipe.js
+++ src/screens/Recipe.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { createStackNavigator } from "react-navigation";
+import { connect } from "react-redux";
 import {
   Container,
   Header,
@@ -8,7 +9,6 @@ import {
   Button,
   Icon,
   Left,
-  Right,
   Body,
   Text,
   Card,
@@ -20,109 +20,113 @@ import {
   Input
 } from "native-base";
 import { Constants } from "expo";
+import { Spinner } from "../components";
+import { fetchAll } from "../redux/actions/recipeActions";
 import RecipeInfo from "./RecipeInfo";
 import { RECIPE_SCREEN, RECIPE_INFO_SCREEN } from "../consts";
 import { ScrollView } from "react-native";
 
-const items = [
-  {
-    Name: "French Fries",
-    Description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-  },
-  {
-    Name: "Mamaliga",
-    Description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-  },
-  {
-    Name: "Placinte",
-    Description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-  },
-  {
-    Name: "Puli prajite",
-    Description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-  }
-];
+const Recipe = connect(
+  recipe => recipe,
+  { fetchAll }
+)(
+  class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { items: [] };
+    }
 
-const Recipe = () => (
-  <Container style={{ paddingTop: Constants.statusBarHeight }}>
-    <ScrollView>
-      <Header>
-        <Body>
-          <Title>All recipes</Title>
-        </Body>
-      </Header>
+    componentDidMount() {
+      this.props.fetchAll(
+        () => {
+          this.setState({ items: this.props.recipe.items });
+        },
+        () => {}
+      );
+    }
 
-      <Header searchBar rounded>
-        <Item>
-          <Icon name="ios-search" />
-          <Input placeholder="Search" />
-          <Icon name="food-variant" type="MaterialCommunityIcons" />
-        </Item>
-        <Button transparent>
-          <Text>Search</Text>
-        </Button>
-      </Header>
-
-      <Content padder>
-        <Card>
-          <CardItem header>
-            <Text>You cook what you want!</Text>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>If you want to cook, just do IT!</Text>
-            </Body>
-          </CardItem>
-          <CardItem>
-            <Body>
-              <Text>Before you start to cook, check your food.</Text>
-            </Body>
-          </CardItem>
-          <CardItem footer>
-            <Text>Enjoy your meal :)</Text>
-          </CardItem>
-        </Card>
-      </Content>
-
-      <List
-        style={{ padding: 5 }}
-        dataArray={items}
-        renderRow={item => (
-          <Card style={{ flex: 0 }}>
-            <CardItem>
-              <Left>
-                <Thumbnail source={require("../../assets/icon.png")} />
-                <Body>
-                  <Text>What Can I Cook?</Text>
-                  <Text note>April 6, 2019</Text>
-                </Body>
-              </Left>
-            </CardItem>
-            <CardItem>
-              <H1>{item.Name}</H1>
-            </CardItem>
-            <CardItem>
+    render() {
+      return recipe.fetchAll.loading ? (
+        <Spinner />
+      ) : (
+        <Container style={{ paddingTop: Constants.statusBarHeight }}>
+          <ScrollView>
+            <Header>
               <Body>
-                <Text>{item.Description}</Text>
+                <Title>All recipes</Title>
               </Body>
-            </CardItem>
-            <CardItem>
-              <Left>
-                <Button transparent textStyle={{ color: "#87838B" }}>
-                  <Icon type="Feather" name="more-vertical" />
-                  <Text>Check menu</Text>
-                </Button>
-              </Left>
-            </CardItem>
-          </Card>
-        )}
-      />
-    </ScrollView>
-  </Container>
+            </Header>
+
+            <Header searchBar rounded>
+              <Item>
+                <Icon name="ios-search" />
+                <Input placeholder="Search" />
+                <Icon name="food-variant" type="MaterialCommunityIcons" />
+              </Item>
+              <Button transparent>
+                <Text>Search</Text>
+              </Button>
+            </Header>
+
+            <Content padder>
+              <Card>
+                <CardItem header>
+                  <Text>You cook what you want!</Text>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>If you want to cook, just do IT!</Text>
+                  </Body>
+                </CardItem>
+                <CardItem>
+                  <Body>
+                    <Text>Before you start to cook, check your food.</Text>
+                  </Body>
+                </CardItem>
+                <CardItem footer>
+                  <Text>Enjoy your meal :)</Text>
+                </CardItem>
+              </Card>
+            </Content>
+
+            <List
+              style={{ padding: 5 }}
+              dataArray={this.state.items}
+              renderRow={item => (
+                <Card style={{ flex: 0 }}>
+                  <CardItem>
+                    <Left>
+                      <Thumbnail source={require("../../assets/icon.png")} />
+                      <Body>
+                        <Text>What Can I Cook?</Text>
+                        <Text note>April 6, 2019</Text>
+                      </Body>
+                    </Left>
+                  </CardItem>
+                  <CardItem>
+                    <H1>{item.name}</H1>
+                  </CardItem>
+                  <CardItem>
+                    <Body>
+                      <Text>{item.description}</Text>
+                    </Body>
+                  </CardItem>
+                  <CardItem>
+                    <Left>
+                      <Button transparent textStyle={{ color: "#87838B" }}>
+                        <Icon type="Feather" name="more-vertical" />
+                        <Text>Check menu</Text>
+                      </Button>
+                    </Left>
+                  </CardItem>
+                </Card>
+              )}
+            />
+          </ScrollView>
+        </Container>
+      );
+    }
+  }
 );
 
 export default createStackNavigator(
