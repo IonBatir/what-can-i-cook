4928d4d0bdb572c3f30a503301952051c9ee1e5a
Ion Batîr <ionbatir@gmail.com>
2019-04-06 17:29:00 +0300

    Fetch food

--- src/screens/Food.js
+++ src/screens/Food.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import {
   Container,
   Header,
@@ -13,119 +14,88 @@ import {
 } from "native-base";
 import { createStackNavigator } from "react-navigation";
 import { Constants } from "expo";
-import { ScrollView, ListView, Alert } from "react-native";
+import { ScrollView, ListView } from "react-native";
+import { Spinner } from "../components";
+import { fetchAll } from "../redux/actions/foodActions";
 import FoodInfo from "./FoodInfo";
 import { FOOD_SCREEN, FOOD_INFO_SCREEN } from "../consts";
 
-const datas = [
-  {
-    Name: "Potato",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  },
-  {
-    Name: "Orange",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  },
-  {
-    Name: "Banana",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  },
-  {
-    Name: "Mango",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  },
-  {
-    Name: "Avocado",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  },
-  {
-    Name: "Lemon",
-    expire_data: "06/06/2019",
-    quantity: "3",
-    uniti: "kg"
-  }
-];
+const Food = connect(
+  food => food,
+  { fetchAll }
+)(
+  class extends Component {
+    constructor(props) {
+      super(props);
+      this.ds = new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      });
+      this.state = {
+        basic: true,
+        listViewData: []
+      };
+    }
 
-class Food extends Component {
-  constructor(props) {
-    super(props);
-    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
-    this.state = {
-      basic: true,
-      listViewData: datas
-    };
-  }
-  deleteRow(secId, rowId, rowMap) {
-    rowMap[`${secId}${rowId}`].props.closeRow();
-    const newData = [...this.state.listViewData];
-    newData.splice(rowId, 1);
-    this.setState({ listViewData: newData });
-  }
-  render() {
-    return (
-      <Container style={{ paddingTop: Constants.statusBarHeight }}>
-        <ScrollView>
-          <Header>
-            <Body>
-              <Title>Food</Title>
-            </Body>
-          </Header>
+    componentDidMount() {
+      this.props.fetchAll(
+        () => {
+          this.setState({ listViewData: this.props.food.items });
+        },
+        () => {}
+      );
+    }
 
-          <Content>
-            <List
-              dataSource={this.ds.cloneWithRows(this.state.listViewData)}
-              renderRow={data => (
-                <ListItem style={{ paddingLeft: 20 }}>
-                  <Text>{data.Name}</Text>
-                </ListItem>
-              )}
-              renderLeftHiddenRow={data => (
-                <Button
-                  full
-                  onPress={() => alert(data.Name)}
-                  style={{
-                    backgroundColor: "#CCC",
-                    flex: 1,
-                    alignItems: "center",
-                    justifyContent: "center"
-                  }}
-                >
-                  <Icon active name="information-circle" />
-                </Button>
-              )}
-              renderRightHiddenRow={(data, secId, rowId, rowMap) => (
-                <Button
-                  full
-                  danger
-                  onPress={_ => this.deleteRow(secId, rowId, rowMap)}
-                  style={{
-                    flex: 1,
-                    alignItems: "center",
-                    justifyContent: "center"
-                  }}
-                >
-                  <Icon active name="trash" />
-                </Button>
-              )}
-              leftOpenValue={75}
-              rightOpenValue={-75}
-            />
-          </Content>
-        </ScrollView>
-      </Container>
-    );
+    deleteRow(secId, rowId, rowMap) {
+      rowMap[`${secId}${rowId}`].props.closeRow();
+      const newData = [...this.state.listViewData];
+      newData.splice(rowId, 1);
+      this.setState({ listViewData: newData });
+    }
+    render() {
+      return food.fetchAll.loading ? (
+        <Spinner />
+      ) : (
+        <Container style={{ paddingTop: Constants.statusBarHeight }}>
+          <ScrollView>
+            <Header>
+              <Body>
+                <Title>Food</Title>
+              </Body>
+            </Header>
+
+            <Content>
+              <List
+                disableRightSwipe
+                dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+                renderRow={data => (
+                  <ListItem style={{ paddingLeft: 20 }}>
+                    <Text>{data.name}</Text>
+                  </ListItem>
+                )}
+                renderRightHiddenRow={(data, secId, rowId, rowMap) => (
+                  <Button
+                    full
+                    danger
+                    onPress={_ => this.deleteRow(secId, rowId, rowMap)}
+                    style={{
+                      flex: 1,
+                      alignItems: "center",
+                      justifyContent: "center"
+                    }}
+                  >
+                    <Icon active name="trash" />
+                  </Button>
+                )}
+                leftOpenValue={75}
+                rightOpenValue={-75}
+              />
+            </Content>
+          </ScrollView>
+        </Container>
+      );
+    }
   }
-}
+);
 
 export default createStackNavigator(
   {
