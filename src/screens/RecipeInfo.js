a7ad2da90d7f603aba175b99d7eb2ec09c9c0f00
Apxutektor <tatardaniel10@gmail.com>
2019-04-06 22:31:04 +0300

    change page recipe info

--- src/screens/RecipeInfo.js
+++ src/screens/RecipeInfo.js
@@ -1,4 +1,67 @@
-import React from "react";
-import { Text } from "native-base";
+import React, { Component } from "react";
+import { createStackNavigator } from "react-navigation";
+import { connect } from "react-redux";
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Text,
+  Card,
+  CardItem,
+  Thumbnail,
+  H1,
+  List,
+  Item,
+  Input
+} from "native-base";
+import { Constants } from "expo";
+import { Spinner } from "../components";
+import { fetchAll } from "../redux/actions/recipeActions";
+import RecipeInfo from "./RecipeInfo";
+import { RECIPE_SCREEN, RECIPE_INFO_SCREEN } from "../consts";
+import { ScrollView, RefreshControl } from "react-native";
 
-export default () => <Text>RecipeInfo</Text>;
+export default class extends Component {
+  constructor(props) {
+    super(props);
+    const { params } = this.props.navigation.state;
+    this.state = {
+      Name: params.data.Name,
+      Description: params.data.Description
+    };
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { Name, Description } = this.state;
+
+    return (
+      <Container>
+        <Card style={{ flex: 0 }}>
+          <CardItem>
+            <Left>
+              <Thumbnail source={require("../../assets/icon.png")} />
+              <Body>
+                <Text>What Can I Cook?</Text>
+                <Text note>April 6, 2019</Text>
+              </Body>
+            </Left>
+          </CardItem>
+          <CardItem>
+            <H1>{Name}</H1>
+          </CardItem>
+          <CardItem>
+            <Body>
+              <Text>{Description}</Text>
+            </Body>
+          </CardItem>
+        </Card>
+      </Container>
+    );
+  }
+}
