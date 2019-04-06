405006d9a46a723a5dd4f063b7bf3f5864f89942
Ion Batîr <ionbatir@gmail.com>
2019-04-07 02:31:49 +0300

    FoodInfo hotfix

--- src/screens/FoodInfo.js
+++ src/screens/FoodInfo.js
@@ -32,166 +32,166 @@ export default connect(
 
       this.state = this.editMode
         ? {
-          name: params.data.name,
-          expire_date: new Date(params.data.expire_date),
-          quantity: params.data.quantity,
-          uniti: params.data.uniti
-        }
+            name: params.data.name,
+            expire_date: new Date(params.data.expire_date),
+            quantity: params.data.quantity,
+            uniti: params.data.uniti
+          }
         : {
-          name: "",
-          expire_date: new Date(),
-          quantity: "",
-          uniti: ""
-        };
+            name: "",
+            expire_date: new Date(),
+            quantity: "",
+            uniti: ""
+          };
     }
 
     static navigationOptions = ({ navigation }) => ({
-      headerTitle: ${ navigation.state.params.title }
+      headerTitle: `${navigation.state.params.title}`
     });
 
-render() {
-  const {
-    navigation,
-    food,
-    addFood,
-    editFood,
-    deleteFood,
-    fetchAll
-  } = this.props;
-  const { name, quantity, uniti, expire_date } = this.state;
+    render() {
+      const {
+        navigation,
+        food,
+        addFood,
+        editFood,
+        deleteFood,
+        fetchAll
+      } = this.props;
+      const { name, quantity, uniti, expire_date } = this.state;
 
-  const buttonDisabled =
-    name.length < 1 || quantity.length < 1 || uniti.length < 1;
+      const buttonDisabled =
+        name.length < 1 || quantity.length < 1 || uniti.length < 1;
 
-  return food.fetchAll.loading ||
-    food.add.loading ||
-    food.edit.loading ||
-    food.delete.loading ? (
-      <Spinner />
-    ) : (
-      <Container>
-        <ScrollView>
-          <Form>
-            <Item stackedLabel>
-              <Label>Name</Label>
-              <Input
-                onChangeText={text => this.setState({ name: text })}
-                defaultValue={name}
-              />
-            </Item>
-            <Item stackedLabel>
-              <Label>Expire Date</Label>
-              <DatePicker
-                defaultDate={expire_date}
-                minimumDate={new Date()}
-                maximumDate={new Date(2020, 12, 31)}
-                locale={"en"}
-                timeZoneOffsetInMinutes={undefined}
-                modalTransparent={false}
-                animationType={"fade"}
-                androidMode={"default"}
-                onDateChange={date => this.setState({ expire_date: date })}
-                disabled={false}
-              />
-            </Item>
-            <Item stackedLabel>
-              <Label>Quantity</Label>
-              <Input
-                onChangeText={text => this.setState({ quantity: text })}
-                defaultValue={quantity}
-              />
-            </Item>
-            <Item stackedLabel>
-              <Label>Unity</Label>
-              <Input
-                onChangeText={text => this.setState({ uniti: text })}
-                defaultValue={uniti}
-              />
-            </Item>
-          </Form>
+      return food.fetchAll.loading ||
+        food.add.loading ||
+        food.edit.loading ||
+        food.delete.loading ? (
+        <Spinner />
+      ) : (
+        <Container>
+          <ScrollView>
+            <Form>
+              <Item stackedLabel>
+                <Label>Name</Label>
+                <Input
+                  onChangeText={text => this.setState({ name: text })}
+                  defaultValue={name}
+                />
+              </Item>
+              <Item stackedLabel>
+                <Label>Expire Date</Label>
+                <DatePicker
+                  defaultDate={expire_date}
+                  minimumDate={new Date()}
+                  maximumDate={new Date(2020, 12, 31)}
+                  locale={"en"}
+                  timeZoneOffsetInMinutes={undefined}
+                  modalTransparent={false}
+                  animationType={"fade"}
+                  androidMode={"default"}
+                  onDateChange={date => this.setState({ expire_date: date })}
+                  disabled={false}
+                />
+              </Item>
+              <Item stackedLabel>
+                <Label>Quantity</Label>
+                <Input
+                  onChangeText={text => this.setState({ quantity: text })}
+                  defaultValue={quantity}
+                />
+              </Item>
+              <Item stackedLabel>
+                <Label>Unity</Label>
+                <Input
+                  onChangeText={text => this.setState({ uniti: text })}
+                  defaultValue={uniti}
+                />
+              </Item>
+            </Form>
 
-          <Button
-            block
-            disabled={buttonDisabled}
-            success
-            onPress={() => {
-              this.editMode
-                ? editFood(
-                  { ...this.state },
-                  () => {
-                    fetchAll(
-                      () => {
-                        navigation.navigate(FOOD_SCREEN);
-                      },
-                      () => { }
-                    );
-                  },
-                  () => { }
-                )
-                : addFood(
-                  { ...this.state },
-                  () => {
-                    fetchAll(
-                      () => {
-                        navigation.navigate(FOOD_SCREEN);
-                      },
-                      () => { }
-                    );
-                  },
-                  () => { }
-                );
-            }}
-            style={
-              buttonDisabled
-                ? {
-                  margin: 10,
-                  marginTop: 20,
-                  backgroundColor: "gray",
-                  borderRadius: 0
-                }
-                : {
-                  backgroundColor: "#3aafa9",
-                  margin: 10,
-                  marginTop: 20,
-                  borderRadius: 0
-                }
-            }
-          >
-            <Text
-              style={{
-                fontWeight: "bold"
-              }}
-            >
-              Save
-              </Text>
-          </Button>
-          {this.editMode && (
             <Button
               block
-              danger
-              onPress={() =>
-                deleteFood({ name: this.state.name }, () => { }, () => { })
-              }
-              style={{
-                backgroundColor: "white",
-                margin: 10,
-                marginTop: 20,
-                elevation: 0
+              disabled={buttonDisabled}
+              success
+              onPress={() => {
+                this.editMode
+                  ? editFood(
+                      { ...this.state },
+                      () => {
+                        fetchAll(
+                          () => {
+                            navigation.navigate(FOOD_SCREEN);
+                          },
+                          () => {}
+                        );
+                      },
+                      () => {}
+                    )
+                  : addFood(
+                      { ...this.state },
+                      () => {
+                        fetchAll(
+                          () => {
+                            navigation.navigate(FOOD_SCREEN);
+                          },
+                          () => {}
+                        );
+                      },
+                      () => {}
+                    );
               }}
+              style={
+                buttonDisabled
+                  ? {
+                      margin: 10,
+                      marginTop: 20,
+                      backgroundColor: "gray",
+                      borderRadius: 0
+                    }
+                  : {
+                      backgroundColor: "#3aafa9",
+                      margin: 10,
+                      marginTop: 20,
+                      borderRadius: 0
+                    }
+              }
             >
               <Text
                 style={{
-                  color: "grey",
-                  textDecorationLine: "underline"
+                  fontWeight: "bold"
                 }}
               >
-                Remove item
-                </Text>
+                Save
+              </Text>
             </Button>
-          )}
-        </ScrollView>
-      </Container>
-    );
-}
+            {this.editMode && (
+              <Button
+                block
+                danger
+                onPress={() =>
+                  deleteFood({ name: this.state.name }, () => {}, () => {})
+                }
+                style={{
+                  backgroundColor: "white",
+                  margin: 10,
+                  marginTop: 20,
+                  elevation: 0
+                }}
+              >
+                <Text
+                  style={{
+                    color: "grey",
+                    textDecorationLine: "underline"
+                  }}
+                >
+                  Remove item
+                </Text>
+              </Button>
+            )}
+          </ScrollView>
+        </Container>
+      );
+    }
   }
 );
