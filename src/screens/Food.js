d63df3e084e3a32eabf3f7a9543773f04a6e6e0d
AndreiDarii <andrei_darii@yahoo.com>
2019-04-06 17:55:25 +0300

    Adde Buline

--- src/screens/Food.js
+++ src/screens/Food.js
@@ -51,6 +51,22 @@ const Food = connect(
       newData.splice(rowId, 1);
       this.setState({ listViewData: newData });
     }
+
+    ChooseColor(expira){
+      const colors = ["#35aa49", "#c7c22e", "#c43434"]
+      const today = new Date();
+      console.log("Today:", today);
+      console.log("Expire date: ", expira)
+      const difference = expira - today;
+      const days = difference / (3600 * 1000 * 24);
+      console.log("\nDays remaining: ", days);
+      if (days < 0)
+        return colors[2];
+      if (days < 5)
+        return colors[1];
+      else
+        return colors[0];
+    } 
     render() {
       return food.fetchAll.loading ? (
         <Spinner />
@@ -64,31 +80,39 @@ const Food = connect(
             </Header>
 
             <Content>
-              <List
-                disableRightSwipe
-                dataSource={this.ds.cloneWithRows(this.state.listViewData)}
-                renderRow={data => (
-                  <ListItem style={{ paddingLeft: 20 }}>
-                    <Text>{data.name}</Text>
-                  </ListItem>
-                )}
-                renderRightHiddenRow={(data, secId, rowId, rowMap) => (
-                  <Button
-                    full
-                    danger
-                    onPress={_ => this.deleteRow(secId, rowId, rowMap)}
-                    style={{
-                      flex: 1,
-                      alignItems: "center",
-                      justifyContent: "center"
-                    }}
-                  >
-                    <Icon active name="trash" />
-                  </Button>
-                )}
-                leftOpenValue={75}
-                rightOpenValue={-75}
-              />
+            <List
+              dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+              disableLeftSwipe
+              renderRow={data => (
+                <ListItem style={{ 
+                  paddingLeft: 20,
+                  borderBottomColor: "#bbb",
+                  backgroundColor: "#fff"}}>
+                   <Icon style={{color: this.ChooseColor(new Date(data.expire_data))}}
+                     name="circle"
+                     type="FontAwesome"
+                     size={4}
+                   />
+                  <Text style={{color: "#17252a",fontSize: 15, paddingLeft: 10}}>{data.Name}</Text>
+                </ListItem>
+              )}
+              renderRightHiddenRow={(data, secId, rowId, rowMap) => (
+                <Button
+                  full
+                  danger
+                  onPress={_ => this.deleteRow(secId, rowId, rowMap)}
+                  style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center"
+                  }}
+                >
+                  <Icon active name="trash" />
+                </Button>
+              )}
+              leftOpenValue={75}
+              rightOpenValue={-75}
+            />
             </Content>
           </ScrollView>
         </Container>
