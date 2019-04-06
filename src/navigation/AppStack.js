a40711ca17bfa7e4c632fcc98af260d1234211bc
Ion Batîr <ionbatir@gmail.com>
2019-04-06 12:16:48 +0300

    Add tabBar icons

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -1,30 +1,62 @@
+import React from "react";
 import { createBottomTabNavigator } from "react-navigation";
 import { Dashboard, Food, Recipe } from "../screens";
 import { DASHBOARD_SCREEN, FOOD_SCREEN, RECIPE_SCREEN } from "../consts";
+import { Icon } from "native-base";
 
 export default createBottomTabNavigator(
   {
     [DASHBOARD_SCREEN]: {
       screen: Dashboard,
       navigationOptions: {
-        tabBarLabel: "Dashboard"
+        tabBarLabel: "Dashboard",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color="#900" />
+        )
       }
     },
     [FOOD_SCREEN]: {
       screen: Food,
       navigationOptions: {
-        tabBarLabel: "Food"
+        tabBarLabel: "Food",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="food-apple"
+            type="MaterialCommunityIcons"
+            size={25}
+            color="#900"
+          />
+        )
       }
     },
     [RECIPE_SCREEN]: {
       screen: Recipe,
       navigationOptions: {
-        tabBarLabel: "Recipe"
+        tabBarLabel: "Recipe",
+        tabBarIcon: ({ tintColor }) => (
+          <Icon
+            name="food-variant"
+            type="MaterialCommunityIcons"
+            size={25}
+            color="#900"
+          />
+        )
       }
     }
   },
   {
-    navigationOptions: {},
+    navigationOptions: ({ navigation }) => ({
+      tabBarOptions: {
+        showIcon: true,
+        activeTintColor: "white",
+        inactiveTintColor: "gray",
+        showLabel: true,
+        style: {
+          paddingTop: 5,
+          elevation: 0
+        }
+      }
+    }),
     initialRouteName: DASHBOARD_SCREEN,
     animationEnabled: true
   }
